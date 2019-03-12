d7df790c0953918c94681ac2157a7ccd577aa2f9
Luigi Bonati <bonatil@eu-login-25-ng.euler.ethz.ch>
2019-03-12 15:34:05 +0100

    new config instructions

--- config-with-pytorch.sh
+++ config-with-pytorch.sh
@@ -1,24 +1,16 @@
-#fast and working (RGP CLUSTER)
-./configure 	--enable-modules=+ves:+crystallization \
-		CXXFLAGS="-O3 -D_GLIBCXX_USE_CXX11_ABI=0" \
-		CPPFLAGS="-I/home/bonatl/Software/libtorch/include/torch/csrc/api/include/ -I/home/bonatl/Software/libtorch/include/" \
-		LDFLAGS="-L/home/bonatl/Software/libtorch/lib -ltorch -lcaffe2 -lc10" \
+#where to install LIBTORCH
+LIBTORCH=/cluster/home/bonatil/Software/libtorch
 
+if [ ! -d "$LIBTORCH" ]; then
+  wget https://download.pytorch.org/libtorch/nightly/cpu/libtorch-shared-with-deps-latest.zip
+  unzip libtorch-shared-with-deps-latest.zip
+  mv libtorch $LIBTORCH
+  rm libtorch-shared-with-deps-latest.zip
+fi
 
-# -------------------------------------------------------------------------------------------------------
-
-#general setup
-
-#where to install plumed
-PREF=/local/bonatl/Software/plumed2
-
-#assuming that libtorch-shared-with-deps-latest has been downloaded and unzipped in $LIBTORCH
-LIBTORCH=/home/bonatl/Software/libtorch
-
-#./configure 	--prefix=${PREF} \
-#		--enable-modules=+ves:+crystallization \
-#		CXXFLAGS="-O3 -D_GLIBCXX_USE_CXX11_ABI=0" \
-#		CPPFLAGS="-I${LIBTORCH}/include/torch/csrc/api/include/ -I${LIBTORCH}/include/" \
-#		LDFLAGS="-L${LIBTORCH}/lib -ltorch -lcaffe2 -lc10"
-
+./configure     --enable-modules=+ves:+crystallization \
+                --enable-mpi --disable-openmp --enable-rpath CXX=mpic++ CC=mpicc \
+                CXXFLAGS="-O3 -D_GLIBCXX_USE_CXX11_ABI=0" \
+                CPPFLAGS="-I${LIBTORCH}/include/torch/csrc/api/include/ -I${LIBTORCH}/include/ -I${LIBTORCH}/include/torch" \
+                LDFLAGS="-L${LIBTORCH}/lib -ltorch -lcaffe2 -lc10 -Wl,rpath,${LIBTORCH}/lib"
 
