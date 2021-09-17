82215157be23e63df6c2618df22a1177b209cf35
Luigi Bonati <lui.bonati@gmail.com>
2021-09-17 02:08:04 +0200

    added regtest for torch

---
+++ regtest/torch/rt-pytorch_model-2d/create-pytorch-model.py
@@ -0,0 +1,51 @@
+import torch
+print(torch.__version__)
+
+def my_torch_cv(x):
+    '''
+    Here goes the definition of the CV.
+
+    Inputs:
+        x (torch.tensor): input, either scalar or 1-D array
+    Return:
+        y (torch.tensor): collective variable (scalar)
+    '''
+    # CV definition
+    y1 = torch.sin(x)
+    y2 = torch.cos(x)
+    y = torch.cat((y1,y2))
+    return y
+
+input_size = 1
+
+# -- DEFINE INPUT -- 
+#random 
+#x = torch.rand(input_size, dtype=torch.float32, requires_grad=True).unsqueeze(0)
+#or by choosing the value(s) of the array
+x = torch.tensor([0.], dtype=torch.float32, requires_grad=True)
+
+# -- CALCULATE CV -- 
+y = my_torch_cv(x)
+
+# -- CALCULATE DERIVATIVES -- 
+for yy in y:
+    dy = torch.autograd.grad(yy, x, create_graph=True)
+    # -- PRINT -- 
+    print('CV TEST')
+    print('n_input\t: {}'.format(input_size))
+    print('x\t: {}'.format(x))
+    print('cv\t: {}'.format(yy))
+    print('der\t: {}'.format(dy))
+
+# Compile via tracing
+traced_cv   = torch.jit.trace ( my_torch_cv, example_inputs=x )
+# Compile via scripting
+#scripted_cv = torch.jit.script( my_torch_cv )
+
+filename='torch_model.pt'
+traced_cv.save(filename)
+
+# -- SAVE SERIALIZED FUNCTION -- 
+#filename='torch_cv_scripted.pt'
+#scripted_cv.save(filename)
+ 
